19ed804f94c14a90357ca8025d37534753d844c1
Tomgalanx <thomas.gerphanion1@etu.univ-lorraine.fr>
2020-01-20 21:53:49 +0100

    Début projet

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        BigInteger test = new BigInteger("234567834567123456234562345672345672345672345672345611234567812345678123456709549109549109123456789023456782345678549109549109549467835678998765678907890567878941",10);
+
+        System.out.println(test.bitLength());
+    }
+}
